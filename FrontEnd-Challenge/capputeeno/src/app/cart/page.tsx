40ff35ccb6a4f5ff228a80182932681409d24622
Gabriel Modesto <gmodesto2012@gmail.com>
2024-01-15 12:25:18 -0300

    min-wi

--- FrontEnd-Challenge/capputeeno/src/app/cart/page.tsx
+++ FrontEnd-Challenge/capputeeno/src/app/cart/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { BackBtn } from "@/components/back-button";
-import CartItem from "@/components/cart/Cart-item";
+import CartItem from "@/components/cart/cart-item";
 import { DefaultPageLayout } from "@/components/default-page-layout";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { ProductInCart } from "@/types/product";
@@ -10,19 +10,27 @@ import { styled } from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  align-items: flex-start;
-  flex-direction: column;
+  justify-content: center;
+  flex-direction: row;
+  gap: 32px;
+
+  @media(min-width: ${props => props.theme.desktopBreakpoint}){
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    lex-direction: column;
+    height: 100% important;
+
+  }
 `
 
 const CartListContainer = styled.div`
-  flex-direction: column;
-  margin-top: 22px;
-
   h3 {
     font-size: 24px;
     text-transform: uppercase;
     color: var(--text-dark-2);
     line-height: 150%;
+    margin-top: 22px;
   }
 
   p{
@@ -39,30 +47,68 @@ const CartLis = styled.ul`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  gap: 16px;
+  margin-top: 24px;
+`
+
+const CartResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  background: white;
+  height: 100% important;
+
 `
 
 export default function CartPage(){
-    const { value } = useLocalStorage<ProductInCart[]>("cart-items", [])
+    const { value, updatedLocalStorage } = useLocalStorage<ProductInCart[]>("cart-items", [])
 
     const calculateTotal = (value: ProductInCart[]) => {
       return value.reduce((sum, item) => sum += item.price_in_cents * item.quantity, 0)
     }
 
     const cartTotal = formatPrice(calculateTotal(value))
+
+    const handleUpdateQuantity = (id: string, quantity: number) => {
+      const newValue = value.map(item => {
+        if (item.id !== id) return item
+        return {...item, quantity: quantity}
+      })
+      updatedLocalStorage(newValue)
+    }
+
+    const handleDeleteItem = (id:string) => {
+      const newValue = value.filter(item => {
+        if (item.id !== id) return item
+      })
+      updatedLocalStorage(newValue)
+    }
+
     return(
       <DefaultPageLayout>
         <Container>
-        <BackBtn navigate="/"/>
-        <CartListContainer>
-          <h3>Seu Carrinho</h3>
-          <p>
-            Total {value.length} produtos 
-            <span>{cartTotal}</span>
-          </p>
-          <CartLis>
-                {value.map(item => <CartItem product={item} key={item.id}/>)}
-          </CartLis>
-        </CartListContainer>
+          <CartListContainer>
+          <BackBtn navigate="/"/>
+            <h3>Seu Carrinho</h3>
+            <p>
+              Total {value.length} produtos
+              <span> {cartTotal}</span>
+            </p>
+            <CartLis>
+                  {value.map(item => 
+                  <CartItem 
+                    product={item} 
+                    key={item.id} 
+                    handleUpdateQuantity={handleUpdateQuantity}
+                    handleDelete={handleDeleteItem}
+                  />)}
+            </CartLis>
+          </CartListContainer>
+          <CartResultContainer>
+            <h1>Ola</h1>
+          </CartResultContainer>
         </Container>
       </DefaultPageLayout>
     )
--- FrontEnd-Challenge/capputeeno/src/components/cart/cart-item.tsx
+++ FrontEnd-Challenge/capputeeno/src/components/cart/cart-item.tsx
@@ -1,26 +1,112 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { ProductInCart } from "@/types/product"
+import { formatPrice } from "@/utils/format-price"
+import { ChangeEvent } from "react"
 import styled from "styled-components"
+import { DeleteIcon } from "../icons/delete-icon"
 
 interface CartItemProps {
   product: ProductInCart
+  handleUpdateQuantity(id: string, quantity: number): void
+  handleDelete(id: string): void
 }
 
 const Item = styled.li`
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 210px;
+
+  border-radius: 8px;
+  background-color: #ffffff;
+  position: relative;
+
+  button {
+    position: absolute;
+    top: 16px;
+    right: 24px;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+  }
+
+  img {
+    max-height: 100%; 
+    width: 256px;
+    border-radius: 8px 0 0 8px;
+  }
+
+  > div {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-direction: column;
+    padding: 16px 24px;
+    line-height: 150%;
+    color: var(--text-dark-2);
+    
+    h4 {
+      font-weight: 300;
+      font-size: 20px;
+    }
+
+    p {
+      font-weight: 400;
+      font-size: 12px;
+    }
+    
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+
+      span {
+        font-weight: 600;
+        font-size: 16px;
+        color: var(--shapes-dark);
+      }
+    }
+  }
 `
 
-export default function CartItem({ product } : CartItemProps){
+const SelecQuantity = styled.select`
+  padding: 8px;
+  border: 1.5px solid var(--border-color);
+  border-radius: 8px;
+  background-color: var(--bg-secundary);
+
+  color: var(--text-dark);
+  font-size: 16px;
+  font-weight: 400;
+`
+
+export default function CartItem({ product, handleUpdateQuantity, handleDelete } : CartItemProps){
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    handleUpdateQuantity(product.id, Number(e.target.value))
+  }
+
     return(
         <Item>
+          <button onClick={() => handleDelete(product.id)} aria-label="Deletar">
+            <DeleteIcon />
+          </button>
           <img src={product.image_url} />
           <div>
             <h4>{product.name}</h4>
             <p>{product.description}</p>
             <div>
-              <p>{product.quantity}</p>
-              <p>{product.price_in_cents}</p>
+              <SelecQuantity value={product.quantity} onChange={handleChange}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </SelecQuantity>
+              <span>{formatPrice(product.price_in_cents)}</span>
             </div>
           </div>
         </Item>
