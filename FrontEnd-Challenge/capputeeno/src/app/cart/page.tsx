cfa4250817502a8d4860b0cec4b1eefc1999c8a2
Gabriel Modesto <gmodesto2012@gmail.com>
2024-01-17 00:10:04 -0300

    feat: create resume cart

--- FrontEnd-Challenge/capputeeno/src/app/cart/page.tsx
+++ FrontEnd-Challenge/capputeeno/src/app/cart/page.tsx
@@ -10,17 +10,13 @@ import { styled } from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
-  flex-direction: row;
+  flex-direction: column;
   gap: 32px;
+  height: 100%;
+  
 
   @media(min-width: ${props => props.theme.desktopBreakpoint}){
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    lex-direction: column;
-    height: 100% important;
-
+    flex-direction: row;
   }
 `
 
@@ -28,14 +24,16 @@ const CartListContainer = styled.div`
   h3 {
     font-size: 24px;
     text-transform: uppercase;
-    color: var(--text-dark-2);
+    font-weight: 500;
     line-height: 150%;
-    margin-top: 22px;
+    color: var(--text-dark-2);
+    margin-top: 24px;
   }
 
   p{
     color: var(--textos-dark-2);
     font-size: 16px;
+    font-weight: 300;
     span {
     font-weight: bold;
   }
@@ -55,12 +53,85 @@ const CartLis = styled.ul`
 const CartResultContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  justify-content: space-between;
+  min-width: 352px;
+  padding: 16px 24px;
 
   background: white;
-  height: 100% important;
+  height: 100vh;
 
+`
+
+const ResumeContainer = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  div:nth-child(2) {
+    padding-bottom: 24px;
+    border-bottom: 1px solid var(--shapes-dark-2);
+    padding-top: 12px;
+  }
+
+  div:nth-child(3) {
+    color: var(--text-dark-2);
+    font-weight: 600;
+    margin-top: 8px
+  }
+  
+  section {
+    padding-top: 29px;
+
+  }
+
+  button {
+    width: 100%;
+    height: 44px;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 150%;
+    text-transform: uppercase;
+    color: #F5F5FA;
+
+
+    border: none;
+    border-radius: 4px;
+    background-color: #51B853;
+    mix-blend-mode: multiply;
+
+    margin-top: 40px;
+  }
+  
+  button:hover {
+    cursor: pointer;
+    opacity: 80%;
+    transition: 5ms;
+  }
+  `
+
+const LinksCardResume = styled.div`
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: var(--text-dark);
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 150%;
+  }
+
+  li:not(:last-child){
+    margin-bottom: 12px;
+  }
+
+
+  @media(min-width: ${props => props.theme.tabletBreakpoint}){
+    flex-direction: row;
+  }
 `
 
 export default function CartPage(){
@@ -69,6 +140,8 @@ export default function CartPage(){
     const calculateTotal = (value: ProductInCart[]) => {
       return value.reduce((sum, item) => sum += item.price_in_cents * item.quantity, 0)
     }
+
+    const cartTotalValue = calculateTotal(value)
 
     const cartTotal = formatPrice(calculateTotal(value))
 
@@ -87,6 +160,16 @@ export default function CartPage(){
       updatedLocalStorage(newValue)
     }
 
+    const SubtotalResume = cartTotalValue/100
+     
+    const DeliveryValue = 40
+
+    const Delivery = DeliveryValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const calculateTotalResume = SubtotalResume + DeliveryValue
+
+    const resuemTotal = calculateTotalResume.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
     return(
       <DefaultPageLayout>
         <Container>
@@ -95,11 +178,11 @@ export default function CartPage(){
             <h3>Seu Carrinho</h3>
             <p>
               Total {value.length} produtos
-              <span> {cartTotal}</span>
+              <span>{cartTotal}</span>
             </p>
             <CartLis>
                   {value.map(item => 
-                  <CartItem 
+                  <CartItem
                     product={item} 
                     key={item.id} 
                     handleUpdateQuantity={handleUpdateQuantity}
@@ -108,7 +191,32 @@ export default function CartPage(){
             </CartLis>
           </CartListContainer>
           <CartResultContainer>
-            <h1>Ola</h1>
+            <ResumeContainer>
+            <h3>Resumo do pedido</h3>
+              <section>
+                <div>
+                  <p>Subtotal de produtos</p>
+                  <span>{cartTotal}</span>
+                </div>
+                <div>
+                  <p>Entrega</p>
+                  <span>{Delivery}</span>
+                </div>
+                <div>
+                  <p>Total</p>
+                  <span>{resuemTotal}</span>
+                </div>
+              </section>
+              <button>Finalizar a compra</button>
+            </ResumeContainer>
+            <LinksCardResume>
+              <ul>
+                <li><a href="">Ajuda</a></li>
+                <li><a href="">reembolsos</a></li>
+                <li><a href="">entregas e frete</a></li>
+                <li><a href="">trocas e devoluções</a></li>
+              </ul>
+            </LinksCardResume>
           </CartResultContainer>
         </Container>
       </DefaultPageLayout>
